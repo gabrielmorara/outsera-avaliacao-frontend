977a0296c36e417714e170ce3409328819c45b0b
Gabriel Morara <gabriel.morara@gmail.com>
2025-07-24 10:18:31 -0300

    add mensagem caso nenhum registro seja encontrado

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit } from '@angular/core'; 
-import { MovieService, ProducerInterval, StudioWins, YearWins, Movie } from '../movie.service';
+import { Component, OnInit } from '@angular/core';
+import {
+  MovieService,
+  ProducerInterval,
+  StudioWins,
+  YearWins,
+  Movie,
+} from '../movie.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
   yearsWithMultipleWinners: YearWins[] = [];
@@ -12,19 +18,22 @@ export class DashboardComponent implements OnInit {
   maxInterval: ProducerInterval | null = null;
   minInterval: ProducerInterval | null = null;
   winnersByYear: Movie[] = [];
+  lastSearchedYear: number | null = null;
 
   constructor(private movieService: MovieService) {}
 
   ngOnInit() {
-    this.movieService.getYearsWithMultipleWinners().subscribe((years: YearWins[]) => {
-      this.yearsWithMultipleWinners = years;
-    });
+    this.movieService
+      .getYearsWithMultipleWinners()
+      .subscribe((years: YearWins[]) => {
+        this.yearsWithMultipleWinners = years;
+      });
 
     this.movieService.getTopStudios().subscribe((studios: StudioWins[]) => {
       this.topStudios = studios.slice(0, 3);
     });
 
-    this.movieService.getProducerIntervals().subscribe(intervals => {
+    this.movieService.getProducerIntervals().subscribe((intervals) => {
       this.minInterval = intervals.min[0] || null;
       this.maxInterval = intervals.max[0] || null;
     });
@@ -35,9 +44,11 @@ export class DashboardComponent implements OnInit {
     if (!isNaN(y)) {
       this.movieService.getWinnersByYear(y).subscribe((winners: Movie[]) => {
         this.winnersByYear = winners;
+        this.lastSearchedYear = y;
       });
     } else {
       this.winnersByYear = [];
+      this.lastSearchedYear = null;
     }
   }
 }
